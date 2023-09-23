bcc6ae31399716888a520caa2a952618f2beac38
engineeredbyalex <nonstopzen26@gmail.com>
2023-09-24 00:31:09 +0300

    24.09 rev-2

--- components/landing_page/Skills_Component.js
+++ components/landing_page/Skills_Component.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import Page from '../common/layout/Page'
 import { RevealWrapper } from 'next-reveal'
+import Center from '../common/layout/Center'
 
 function Skills_Component() {
     return (
         <Page background="#fff">
-            <RevealWrapper origin='bottom' delay={200} duration={1000} distance='100px' reset={true} >
-                <h5 className='sec_text_dark uppercase absolute left-[30px] top-[50px]  '>Servicii</h5>
-            </RevealWrapper>
-            <RevealWrapper origin='bottom' delay={200} duration={1000} distance='100px' reset={true} >
-                <h3 className='sec_text_dark uppercase absolute left-[30px] top-[50px]  '>Dezvoltare,design,consultanță și conținut</h3>
-            </RevealWrapper>
+
+            <Center>
+                <div className='mt-[50px]'>
+                    <RevealWrapper origin='bottom' delay={200} duration={1000} distance='300px' reset={true} >
+                        <h4 className='sec_text_dark'>Servicii</h4>
+                    </RevealWrapper>
+                </div>
+                <div className='mt-[20px]'>
+                    <RevealWrapper origin='bottom' delay={400} duration={1000} distance='300px' reset={true} >
+                        <h3 className='sec_text_dark'>Dezvoltare,Design,<br /> Consultanță,Conținut.</h3>
+                    </RevealWrapper>
+                </div>
+            </Center>
 
         </Page>
     )
