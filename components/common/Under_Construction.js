10712f7c8a104ac6ad50fc1e09c3b4ac7e212833
engineeredbyalex <nonstopzen26@gmail.com>
2023-09-28 15:03:55 +0300

    28.09 rev-2

--- components/common/Under_Construction.js
+++ components/common/Under_Construction.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Under_Construction() {
     return (
-        <div className='w-screen h-screen  flex items-center justify-center bg-[#1A1A1A]'>
+        <div className='w-screen h-screen flex items-center justify-center text-center bg-[#1A1A1A]'>
             <h1 className='main_text'>
                 Închis pentru mentenanță :)
             </h1>
