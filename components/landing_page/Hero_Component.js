a3ab42febbe61bd1fd8f9c17103d7238b99bf9ca
engineeredbyalex <nonstopzen26@gmail.com>
2023-10-10 23:56:56 +0300

    10/10 rev-2

--- components/landing_page/Hero_Component.js
+++ components/landing_page/Hero_Component.js
@@ -48,15 +48,15 @@ function Hero_Component() {
 
     return (
         <div className='min-h-screen w-screen bg-[#DBDBDB] overflow-hidden flex items-center justify-center flex-col'>
-            <div className='max-w-[1024px] flex flex-col '>
-                <div className='w-full h-full flex items-end justify-start gap-[20px] flex-col lg:flex-row mt-[10rem] lg:mt-[5rem]'>
+            <div className='max-w-[1024px] flex flex-col ml-[30px] mr-[30px] lg:ml-[0px] lg:mr-[0px]'>
+                <div className='  flex flex-col items-center justify-center gap-[30px] lg:items-end lg:justify-start lg:flex-row mt-[10rem] lg:mt-[5rem]'>
                     <Image width={200} height={200} src={Hero_Image}></Image>
                     <div className='flex gap-[5px] flex-col'>
                         <h4 className='text-[#3f3f3f] font-bold uppercase'>Lăzărescu Alexandru</h4>
                         <p className='text-[#3f3f3f] font-bold uppercase'>Web Developer și Designer UI/UX</p>
                     </div>
                 </div>
-                <div className="h-full w-full flex items-center justify-center lg:items-start lg:justify-start flex-col mt-[10rem]">
+                <div className="h-full w-full flex items-center justify-center lg:items-start lg:justify-start flex-col mt-[10rem] ">
                     <RevealWrapper origin="bottom" duration={500} delay={500} reset={false}>
                         <h4 className=" text-[30px]  uppercase text-[#3F3F3F] mb-8">
                             Proiectele mele
