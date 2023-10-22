7f4f64513c4144099c51109c6a745e65bc3fe58f
engineeredbyalex <nonstopzen26@gmail.com>
2023-10-22 22:40:22 +0300

    22.01 rev-1

---
+++ components/common/buttons/Button.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Button = ({ link, text, type, color }) => {
+    const getButtonStyle = () => {
+        switch (type) {
+            case 'outline':
+                return {
+                    border: `2px solid ${color}`,
+                    color: color,
+                    background: 'none',
+                };
+            case 'transparent':
+                return {
+                    border: 'none',
+                    color: color,
+                    background: 'transparent',
+                };
+            case 'solid':
+                return {
+                    border: `2px solid ${color}`,
+                    color: '#000',
+                    background: color,
+                };
+            default:
+                return {
+                    border: `2px solid ${color}`,
+                    color: color,
+                    background: 'none',
+                };
+        }
+    };
+
+    return (
+        <Link href={link}>
+            <p style={getButtonStyle()} className="button px-3 rounded-2xl text-[25px] lg:text-[30px] flex items-center justify-center text-center uppercase font-bold">
+                {text}
+            </p>
+        </Link>
+    );
+};
+
+export default Button;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Navigation_Bar from "@/components/layout/header/Navigation_Bar";
 import Page from "@/components/layout/page/Page";
 import { RevealWrapper } from "next-reveal";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stage } from "@react-three/drei";
+import Button from "@/components/common/buttons/Button";
+
+import { Model } from "@/components/Iphone";
+
 
 export default function Home() {
   return (
@@ -8,18 +14,39 @@ export default function Home() {
       <Navigation_Bar />
       <Page>
         <div>
+          <div className="absolute right-[-20%] top-0 h-screen w-screen flex items-center justify-center">
+            <RevealWrapper>
+              <div className=" h-screen w-screen  text-white z-[0]">
+                <Canvas>
+                  <Stage environment="studio">
+                    <OrbitControls autoRotate enableZoom={false} />
+                    <ambientLight intensity={0.1} />
+                    <directionalLight position={[1, 1, 1]} intensity={0.1} />
+                    <mesh>
+                      <Model />
+                    </mesh>
+                  </Stage>
+                </Canvas>
+              </div>
+            </RevealWrapper>
+          </div>
           <RevealWrapper>
             <h2 className="text-[#fff] font-light uppercase">Salut !</h2>
           </RevealWrapper>
           <RevealWrapper>
             <h4 className="text-[#fff] font-light uppercase">Eu sunt <span className="text-[#F4E869] font-bold">Alex</span></h4>
           </RevealWrapper>
-          <div className="grid grid-cols-2 lg:flex lg:gap-[30px]">
-            <p className="text-[#F4E869] font-bold uppercase">Student</p>
-            <p className="text-[#F4E869] font-bold uppercase">Web Developer</p>
-            <p className="text-[#F4E869] font-bold uppercase">Designer</p>
+          <div className="flex flex-col lg:flex-row gap-[20px] lg:gap-[30px] ">
+            <p className="text-[#F4E869] font-bold uppercase z-[1] text-[20px]">Student</p>
+            <p className="text-[#F4E869] font-bold uppercase z-[1] text-[20px]">Web Developer</p>
+            <p className="text-[#F4E869] font-bold uppercase z-[1] text-[20px]">Designer</p>
+          </div>
+          <div className="flex absolute bottom-[25%] lg:bottom-[20%] gap-[30px]">
+            <Button link="/projects" text="Proiecte" type="solid" color="#F4E869" />
+            <Button link="/contact" text="Contact" type="outline" color="#F4E869" />
           </div>
         </div>
+
       </Page >
     </div >
   )
