f98467dcbe6da901f763b2afae25c32472cdadc7
engineeredbyalex <nonstopzen26@gmail.com>
2023-10-31 16:20:38 +0200

    31.10 rev-3

--- components/layout/content/TestimonailBox.js
+++ components/layout/content/TestimonailBox.js
@@ -5,7 +5,7 @@ function TestimonailBox(props) {
         <div className='w-full h-auto bg-[#F4E869] flex items-center justify-center flex-col rounded-2xl'>
             <div className='flex flex-col items-start justify-start mb-5 px-3 py-3'>
                 <p className=' font-bold uppercase text-[20px] lg:text-[26px] '>{props.title}</p>
-                <p className='text-[16px] font-semibold uppercase text-left'>"{props.testimonial}" - <span className='font-bold'>{props.client}</span> </p>
+                <p className='text-[16px] font-semibold uppercase text-left'>&quot{props.testimonial}&quot - <span className='font-bold'>{props.client}</span> </p>
             </div>
         </div>
     );
