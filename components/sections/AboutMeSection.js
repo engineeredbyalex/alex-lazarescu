f29d0da2011b23cd5128d1e4e32262b11231234a
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-03-06 01:57:11 +0200

    testing

--- components/sections/AboutMeSection.js
+++ components/sections/AboutMeSection.js
@@ -12,13 +12,13 @@ function AboutMeSection() {
                          <RevealWrapper origin="bottom">
                     <h3 style={{ textTransform: "uppercase", margin: '60px 0px 60px 0px' }}>ABOUT ME</h3>
                 </RevealWrapper>
-           <div style={{display:'flex',flexDirection:'column', alignItems:"center", justifyContent:'center',gap:'5rem'}}>
-                 <h3 style={{ fontWeight: '300', fontStyle: 'italic', fontFamily: "Playfair Display",width:'100%' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', alignItems: "center", justifyContent: 'center', gap: '5rem', fontWeight: '300', fontStyle: 'italic', fontFamily: "Playfair Display",  }}>
+                 <h4 className="text_area">
                     As a seasoned Web Developer Designer with 3 years of experience in building amazing digital experiances, I bring expertise in both branding and art direction to the table. I have a strong passion for UI design, where I aim to create emotionally impactful experiences for users.
-              </h3>
-                 <h3 style={{ fontWeight: '300', fontStyle: 'italic', fontFamily: "Playfair Display",width:'100%' }}>
+              </h4>
+                        <h4 className="text_area">
                             My skills cover a wide range of digital platforms, including mobile apps, SaaS platforms, landing pages, e-commerce sites, web-apps, and much more.
-              </h3>
+              </h4>
                     </div>
                     <div>
                     
