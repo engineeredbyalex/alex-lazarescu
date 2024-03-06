8d87e73bd5900c90220494c8178b852d49e6b27b
engineeredbyalex <dev.lazarescu.alexandru@gmail.com>
2024-03-06 02:32:10 +0200

    testing

--- components/sections/ContactSection.js
+++ components/sections/ContactSection.js
@@ -5,7 +5,7 @@ function ContactSection() {
   return (
       <Page>
           <div className="page_container">
-              <div style={{height:'auto',width:'100%', display:'flex',flexDirection:'column',gap:'0rem',justifyContent:'center',alignItems:'center'}}>
+              <div style={{height:'auto',width:'91vw', display:'flex',flexDirection:'column',gap:'0rem',justifyContent:'center',alignItems:'center'}}>
                   <h1 className='contact_button fill'>LinkedIn</h1>
                   <h1 className='contact_button fill'>Email</h1>
                   <h1 className='contact_button fill'>Instagram</h1>
